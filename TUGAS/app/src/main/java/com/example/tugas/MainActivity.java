bdb900bdd1fd5ffa6e93adbbadd2338a78b09840
Alvrialdo Sanjaya Dese <alvrialdo.sanjaya@ti.ukdw.ac.id>
2020-02-19 18:20:41 +0700

    Login ke halaman Home

--- TUGAS/app/src/main/java/com/example/tugas/MainActivity.java
+++ TUGAS/app/src/main/java/com/example/tugas/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.tugas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText1;
+    EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn_Login);
+        editText1 = findViewById(R.id.txt_edit);
+        editText2 = findViewById(R.id.txt_edit2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editText1.getText().toString();
+                String pass = editText2.getText().toString();
+                Toast.makeText(getApplicationContext(),"Email: " +email+ " Password :" + pass,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, halamanHome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
