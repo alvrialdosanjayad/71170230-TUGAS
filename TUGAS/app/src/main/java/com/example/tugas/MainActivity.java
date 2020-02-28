36e7e96f8330f4d5502afb54f3b4436ae1750b0e
Alvrialdo Sanjaya Dese <alvrialdo.sanjaya@ti.ukdw.ac.id>
2020-02-28 17:09:52 +0700

    Latihan menggunakan branch

--- TUGAS/app/src/main/java/com/example/tugas/MainActivity.java
+++ TUGAS/app/src/main/java/com/example/tugas/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String coba;
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.btn_Login);
         editText1 = findViewById(R.id.txt_edit);
